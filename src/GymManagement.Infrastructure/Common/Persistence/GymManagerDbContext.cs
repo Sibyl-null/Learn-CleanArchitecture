714e5883658327366aad31d794ef282738f5f251
Sibyl-null <1286438810@qq.com>
2024-10-11 23:43:39 +0800

    [feat]: 使用 EF Core, SQLite 作为实际存储方案

---
+++ src/GymManagement.Infrastructure/Common/Persistence/GymManagerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using GymManagement.Domain.Subscriptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagement.Infrastructure.Common.Persistence;
+
+public class GymManagerDbContext : DbContext
+{
+    public DbSet<Subscription> Subscriptions { get; set; } = null!;
+
+    public GymManagerDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
