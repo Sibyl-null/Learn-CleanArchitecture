17859354b5fdfa5493dcc205460d92dc1b179bec
yangboyuan <yangboyuan@dailyinnovation.biz>
2024-10-11 17:49:27 +0800

    [feat]: 导入 MediatR 库，使用 CQRS 和 中介者重构之前的 Service 代码

--- src/GymManagement.Application/Services/SubscriptionsWriteService.cs
+++
@@ -1,9 +0,0 @@
-namespace GymManagement.Application.Services;
-
-internal class SubscriptionsWriteService : ISubscriptionsWriteService
-{
-    public Guid CreateSubscription(string subscriptionType, Guid adminId)
-    {
-        return Guid.NewGuid();
-    }
-}
